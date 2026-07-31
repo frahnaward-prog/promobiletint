79605f3d3c9660b4444d900b6fb64d44da963d1d
frahnaward-prog <frahnaward@gmail.com>
2026-07-31 14:29:25 -0400

    update pricing,about,homepage

--- main.js
+++ main.js
@@ -378,3 +378,54 @@ faqItems.forEach(item => {
     });
 
 });
+/* =====================================
+   GALLERY LIGHTBOX
+===================================== */
+
+const galleryImages =
+document.querySelectorAll(".gallery-item img");
+
+const lightbox =
+document.getElementById("lightbox");
+
+const lightboxImg =
+document.getElementById("lightbox-img");
+
+const closeLightbox =
+document.querySelector(".close-lightbox");
+
+galleryImages.forEach(img=>{
+
+img.addEventListener("click",()=>{
+
+lightbox.style.display="flex";
+
+lightboxImg.src=img.src;
+
+});
+
+});
+
+if(closeLightbox){
+
+closeLightbox.onclick=()=>{
+
+lightbox.style.display="none";
+
+}
+
+}
+
+if(lightbox){
+
+lightbox.onclick=(e)=>{
+
+if(e.target===lightbox){
+
+lightbox.style.display="none";
+
+}
+
+}
+
+}
